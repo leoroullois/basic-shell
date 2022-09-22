0b8179121aa455c438662a50c44d879b0febad41
leoroullois <roullois.leo@protonmail.com>
2022-09-22 17:49:36 +0200

    feat : redirections

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,19 +6,53 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define MAX 50
+#define MAX 1000
 
 int split(char *buffer, char **word) {
   char *t = buffer;
   int i = 0;
-  while (word[i++] = strtok(t, " \f\n\r\t\v"))
+  while ((word[i++] = strtok(t, " \f\n\r\t\v")))
     t = NULL;
   return i;
+}
+
+// STDOUT redirections
+void handleStdoutRedirection(const int i, char *args[]) {
+  args[i] = NULL;
+  close(1);
+  open(args[i + 1], O_WRONLY | O_TRUNC | O_CREAT);
+}
+
+void handleStdoutAppendRedirection(const int i, char *args[]) {
+  args[i] = NULL;
+  close(1);
+  open(args[i + 1], O_WRONLY | O_APPEND | O_CREAT);
+}
+
+// STDERR redirections
+void handleStderrRedirection(const int i, char *args[]) {
+  args[i] = NULL;
+  close(2);
+  open(args[i + 1], O_WRONLY | O_TRUNC | O_CREAT);
+}
+
+void handleStderrAppendRedirection(const int i, char *args[]) {
+  args[i] = NULL;
+  close(2);
+  open(args[i + 1], O_WRONLY | O_APPEND | O_CREAT);
+}
+
+// STDIN redirections
+void handleStdinRedirection(const int i, char *args[]) {
+  args[i] = NULL;
+  close(0);
+  open(args[i + 1], O_RDONLY);
 }
 
 int main(int argc, char *argv[]) {
   int status;
   char command[MAX];
+  char *args[MAX];
 
   while (1) {
     pid_t pid = fork();
@@ -26,20 +61,35 @@ int main(int argc, char *argv[]) {
       perror("fork");
       exit(1);
     case 0:
+      // Prompt
       printf("> ");
       fgets(command, MAX, stdin);
-
-      char *args[MAX];
       split(command, args);
-      execvp(command, args);
-      exit(0);
 
+      int i = 0;
+      while (args[i] != NULL) {
+        if (strcmp(args[i], ">") == 0) {
+          handleStdoutRedirection(i, args);
+        } else if (strcmp(args[i], ">>") == 0) {
+          printf("found");
+          handleStdoutAppendRedirection(i, args);
+        } else if (strcmp(args[i], "<") == 0) {
+          handleStdinRedirection(i, args);
+        } else if (strcmp(args[i], "2>") == 0) {
+          handleStderrRedirection(i, args);
+        } else if (strcmp(args[i], "2>>") == 0) {
+          handleStderrAppendRedirection(i, args);
+        } 
+        i++;
+      }
+      if (execvp(args[0], args) < 0) {
+        printf("Command not found\n");
+      }
+      exit(0);
     default:
       waitpid(pid, &status, 0);
-      break;
+      continue;
     }
   }
-  // execvp("ls", argv);
-  printf("sortie\n");
   return 0;
 }
